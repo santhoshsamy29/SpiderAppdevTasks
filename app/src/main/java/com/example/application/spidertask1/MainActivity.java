2fb6b0257204eacded53e63735f0d436229ebc1f
Santhosh Kumar <santhoshsamy29@gmail.com>
2017-05-30 22:52:29 +0530

    .

--- app/src/main/java/com/example/application/spidertask1/MainActivity.java
+++ app/src/main/java/com/example/application/spidertask1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.application.spidertask1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView text1;
     public int x=0;
+    public static final String PREF = "state";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,37 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text1 = (TextView)findViewById(R.id.text1);
                 x+=1;
                 text1.setText(Integer.toString(x));
             }
         });
+
+        Button button2 = (Button)findViewById(R.id.reset);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                x=0;
+                text1.setText(Integer.toString(x));
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences preferences = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("Number",x);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences preferences = getSharedPreferences(PREF, Context.MODE_PRIVATE);
+        x=preferences.getInt("Number",0);
+
+        text1 = (TextView)findViewById(R.id.text1);
+        text1.setText(Integer.toString(x));
     }
 }
